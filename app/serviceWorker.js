d90c82916963f9a56f086506d1aba8bc0e6958ed
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-04-27 19:38:04 +0200

    Revamp with PWA installation

--- app/serviceWorker.js
+++ app/serviceWorker.js
@@ -1,3 +1,42 @@
+var cacheName = 'hello-pwa';
+var filesToCache = [
+    '/app/offline.html',
+    '/app/manifest.json',
+    '/app/favicon.ico',
+    '/app/assets/images/hello-icon-152.png',
+    '/app/assets/images/hello-icon-192.png'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    // request.mode = navigate isn't supported in all browsers
+    // so include a check for Accept: text/html header.
+    if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        event.respondWith(
+            fetch(event.request.url).catch(error => {
+                // Return the offline page
+                return caches.match('/app/offline.html');
+            })
+        );
+    }
+    else{
+        // Respond with everything else if we can
+        event.respondWith(caches.match(event.request)
+            .then(function (response) {
+                return response || fetch(event.request);
+            })
+        );
+    }
+});
+
 self.addEventListener('push', function (event) {
     if (!(self.Notification && self.Notification.permission === 'granted')) {
         return;
