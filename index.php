36cb1ee99819e3c1b7aa067a0b89e7effc487c64
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-04-27 19:07:36 +0200

    Adds fastroute and webpush

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('POST', '/push-subscription', 'subscription_handler');
+    $r->addRoute('PUT', '/push-subscription', 'subscription_handler');
+    $r->addRoute('DELETE', '/push-subscription', 'subscription_handler');
+    $r->addRoute('POST', '/push-notification', 'notification_handler');
+//    $r->addRoute('GET', '[/]', 'homepage_handler');
+});
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        call_user_func_array($handler, $vars);
+        break;
+}
+
+function notification_handler(){
+    require __DIR__.'/server/notifications/send_push_notification.php';
+    return;
+}
+
+function subscription_handler(){
+    require __DIR__.'/server/notifications/push_subscription.php';
+    return;
+}
+
+function homepage_handler(){
+    require __DIR__.'/app/index.html';
+    return;
+}
