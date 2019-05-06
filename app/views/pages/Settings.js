3cab1926b585853ce5830f1e5565d294536b520c
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-05-06 18:46:09 +0200

    Refactor Settings.js

--- app/views/pages/Settings.js
+++ app/views/pages/Settings.js
@@ -20,6 +20,72 @@ let Settings = {
     }
 
     , after_render: async (event) => {
+        Settings.notificationSubscribeHandler();
+        Settings.notificationTestHandler();
+        Settings.installHandler(event);
+    }
+
+    , installHandler: (event) => {
+        const installMessage = null || document.getElementById('install');
+        const impossibleInstallMessage = null || document.getElementById('impossible-install');
+        let installEvent = event;
+
+        window.addEventListener('beforeinstallprompt', (event) => {
+            // Prevent Chrome 67 and earlier from automatically showing the prompt
+            event.preventDefault();
+            // Update UI notify the user they can add to home screen
+            installMessage.style.display = 'block';
+            // Stash the event so it can be triggered later.
+            installEvent = event;
+        });
+
+        if (installEvent){
+            installMessage.style.display = 'block';
+            impossibleInstallMessage.style.display = 'none';
+        }
+
+        installMessage.addEventListener('click', async (event) => {
+            // Show the prompt
+            installEvent.prompt();
+            // Wait for the user to respond to the prompt
+            let choiceResult = await installEvent.userChoice;
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+                installMessage.style.display = 'none';
+                impossibleInstallMessage.style.display = 'block';
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            installEvent = null;
+        });
+    }
+
+    , notificationTestHandler: () => {
+        /**
+         * START send_push_notification
+         * this part handles the button that calls the endpoint that triggers a notification
+         * in the real world, you wouldn't need this, because notifications are typically sent from backend logic
+         */
+
+        const sendPushButton = null || document.querySelector('#send-push-button');
+
+        sendPushButton.addEventListener('click', async () => {
+            const serviceWorkerRegistration = await navigator.serviceWorker.ready;
+            const subscription = await serviceWorkerRegistration.pushManager.getSubscription();
+            if (!subscription) {
+                alert('Please enable push notifications');
+                return;
+            }
+            const contentEncoding = (PushManager.supportedContentEncodings || ['aesgcm'])[0];
+            const jsonSubscription = subscription.toJSON();
+            fetch('/push-notification', {
+                method: 'POST',
+                body: JSON.stringify(Object.assign(jsonSubscription, {contentEncoding})),
+            });
+        });
+    }
+
+    , notificationSubscribeHandler: async () => {
         const notificationOn = null || document.getElementById('notification-on');
         const notificationOff = null || document.getElementById('notification-off');
         const notificationMessage = null || document.getElementById('notification-message');
@@ -57,65 +123,6 @@ let Settings = {
             notificationOn.style.display = 'none';
             notificationOff.style.display = 'block';
         }
-
-        /**
-         * START send_push_notification
-         * this part handles the button that calls the endpoint that triggers a notification
-         * in the real world, you wouldn't need this, because notifications are typically sent from backend logic
-         */
-
-        const sendPushButton = null || document.querySelector('#send-push-button');
-
-        sendPushButton.addEventListener('click', async () => {
-            const serviceWorkerRegistration = await navigator.serviceWorker.ready;
-            const subscription = await serviceWorkerRegistration.pushManager.getSubscription();
-            if (!subscription) {
-                alert('Please enable push notifications');
-                return;
-            }
-            const contentEncoding = (PushManager.supportedContentEncodings || ['aesgcm'])[0];
-            const jsonSubscription = subscription.toJSON();
-            fetch('/push-notification', {
-                method: 'POST',
-                body: JSON.stringify(Object.assign(jsonSubscription, {contentEncoding})),
-            });
-        });
-
-        /**
-         * END send_push_notification
-         */
-        const installMessage = null || document.getElementById('install');
-        const impossibleInstallMessage = null || document.getElementById('impossible-install');
-        let installEvent = event;
-
-        window.addEventListener('beforeinstallprompt', (event) => {
-            // Prevent Chrome 67 and earlier from automatically showing the prompt
-            event.preventDefault();
-            // Update UI notify the user they can add to home screen
-            installMessage.style.display = 'block';
-            // Stash the event so it can be triggered later.
-            installEvent = event;
-        });
-
-        if (installEvent){
-            installMessage.style.display = 'block';
-            impossibleInstallMessage.style.display = 'none';
-        }
-
-        installMessage.addEventListener('click', async (event) => {
-            // Show the prompt
-            installEvent.prompt();
-            // Wait for the user to respond to the prompt
-            let choiceResult = await installEvent.userChoice;
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-                installMessage.style.display = 'none';
-                impossibleInstallMessage.style.display = 'block';
-            } else {
-                console.log('User dismissed the A2HS prompt');
-            }
-            installEvent = null;
-        });
     }
 };
 
