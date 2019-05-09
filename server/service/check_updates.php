5d408c80072f4fd1835463cb0d37a373c991bd8f
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-05-09 19:00:00 +0200

    Adds file that checks update

---
+++ server/service/check_updates.php
@@ -0,0 +1,72 @@
+<?php
+
+use Minishlink\WebPush\Subscription;
+use Minishlink\WebPush\WebPush;
+use SubitoPuntoItAlert\Api\SubitoUpdater;
+use SubitoPuntoItAlert\Database\Repository\ResearchRepository;
+use SubitoPuntoItAlert\Database\Repository\SubscriptionRepository;
+
+$researchRepository = new ResearchRepository();
+$subscriptionRepository = new SubscriptionRepository();
+$api = new SubitoUpdater();
+$researches = $researchRepository->getAllResearch();
+// TODO: gets this from DB
+$lastCheck = '2019-04-23 15:08:59';
+
+foreach ($researches as $research){
+    $response = $api->getAnnouncementUpdate(
+        $lastCheck,
+        $research->getRegion(),
+        $research->getCity(),
+        $research->getQuery()
+    );
+
+    if (strcmp('new announcements', $response['status']) === false){
+        //TODO log something
+    }
+
+    $subscriptionModel = $subscriptionRepository->getSubscription($research->getEndpoint());
+    $subscription = new Subscription(
+        $subscriptionModel->getEndpoint(),
+        $subscriptionModel->getPublicKey(),
+        $subscriptionModel->getAuthToken(),
+        $subscriptionModel->getContentEncoding()
+    );
+
+    $auth = array(
+        'VAPID' => array(
+            'subject' => 'https://github.com/Minishlink/web-push-php-example/',
+            'publicKey' => file_get_contents('keys/public_key.txt'), // don't forget that your public key also lives in app.js
+            'privateKey' => file_get_contents('keys/private_key.txt'), // in the real world, this would be in a secret file
+        ),
+    );
+
+    $webPush = new WebPush($auth);
+
+    $res = $webPush->sendNotification(
+        $subscription,
+        $response['status']
+    );
+
+    // handle eventual errors here, and remove the subscription from your server if it is expired
+    foreach ($webPush->flush() as $report) {
+        $endpoint = $report->getRequest()->getUri()->__toString();
+
+        if ($report->isSuccess()) {
+            echo "[v] Message sent successfully for subscription {$endpoint}.";
+        } else {
+            echo "[x] Message failed to sent for subscription {$endpoint}: {$report->getReason()}";
+        }
+        if ($report->isSubscriptionExpired()) {
+            $subscriptionRepository->delete($endpoint);
+            foreach ($researchRepository->getResearchesByEndpoint($subscription['endpoint']) as $research){
+                $researchRepository->delete($research);
+            }
+            echo "[x] Subscription expired";
+        }
+        echo "[x] Subscription not expired";
+    }
+
+}
+
+$lastCheck = date("Y-m-d H:i:s");
