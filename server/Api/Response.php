45d30070edfd8b4e543be41b9e984502e4a0c1f2
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-05-21 20:17:09 +0200

    Adds Api Response Class

---
+++ server/Api/Response.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace SubitoPuntoItAlert\Api;
+
+class Response
+{
+    /**
+     * @var int
+     */
+    private $httpCode;
+
+    /**
+     * @var array
+     */
+    private $data;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * Response constructor.
+     * @param int $httpCode
+     * @param array $data
+     * @param string $message
+     */
+    public function __construct(int $httpCode = 500, array $data = null, string $message = 'Just Created')
+    {
+        $this->httpCode = $httpCode;
+        $this->data = $data;
+        $this->message = $message;
+    }
+
+    public function send()
+    {
+        header('Content-Type: application/json');
+        echo json_encode(array('code' => $this->httpCode, 'message' => $this->message, 'data' => $this->data));
+    }
+
+    /**
+     * @return int
+     */
+    public function getHttpCode(): int
+    {
+        return $this->httpCode;
+    }
+
+    /**
+     * @param int $httpCode
+     */
+    public function setHttpCode(int $httpCode): void
+    {
+        $this->httpCode = $httpCode;
+    }
+
+    /**
+     * @return array
+     */
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param array $data
+     */
+    public function setData(array $data): void
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     */
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+    }
+
+}
