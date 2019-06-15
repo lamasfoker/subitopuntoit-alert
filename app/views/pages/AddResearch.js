8d137fb1bf3f8f25849ebf407d052fb8f925c189
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-06-15 19:18:27 +0200

    Improve addResearch form

--- app/views/pages/AddResearch.js
+++ app/views/pages/AddResearch.js
@@ -1,4 +1,5 @@
 import ApiRequest from "../../services/ApiRequest.js";
+import Init       from "../../services/Init.js";
 
 let AddResearch = {
 
@@ -8,20 +9,22 @@ let AddResearch = {
                 <form class="col s12" method="POST" action="" id="add-research-form">
                     <div class="row">
                         <div class="input-field col s12">
-                            <input name="city" id="city" type="text">
-                            <label for="first_name">Provincia</label>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input name="region" id="region" type="text">
-                            <label for="first_name">Regione</label>
+                            <input name="location" id="location" type="text" class="autocomplete">
+                            <label for="first_name">Dove</label>
                         </div>
                     </div>
                     <div class="row">
                         <div class="input-field col s12">
                             <input name="query" id="query" type="text">
                             <label for="first_name">Ricerca</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <label>
+                                <input type="checkbox" class="filled-in" id="only-title"/>
+                                <span>Cerca solo nel Titolo</span>
+                            </label>
                         </div>
                     </div>
                     <div class="row">
@@ -35,29 +38,74 @@ let AddResearch = {
     }
 
     , after_render: async () => {
+        const form = null || document.querySelector('#add-research-form');
+        const headerTitle = null || document.querySelector('#header-title');
+        const autoCompleteLocation = null || document.querySelector('.autocomplete');
+
+        M.Autocomplete.init(autoCompleteLocation);
+        headerTitle.innerText = 'Aggiungi una Ricerca';
+        autoCompleteLocation.oninput = AddResearch.updateAutoCompleteLocation;
+        form.onsubmit = AddResearch.sendResearch;
+    }
+
+    , locations : {}
+
+    , updateAutoCompleteLocation : async (event) => {
+        let inputText = event.target;
+        let autocomplete = M.Autocomplete.getInstance(inputText);
+        let jsonResponse = await ApiRequest.get('/get-location?q=' + inputText.value);
+        if (jsonResponse.code !== 200) {
+            return;
+        }
+
+        let dataLocation = jsonResponse.data;
+        let locations = {};
+        for (let i=0; i<dataLocation.length; i++) {
+            if ('town' in dataLocation[i]) {
+                locations[dataLocation[i].town.value] = null;
+                AddResearch.locations[dataLocation[i].town.value] =
+                    dataLocation[i].region.friendly_name + ' ' +
+                    dataLocation[i].city.friendly_name + ' ' +
+                    dataLocation[i].town.friendly_name;
+                continue;
+            }
+            if ('city' in dataLocation[i]) {
+                locations[dataLocation[i].city.value] = null;
+                AddResearch.locations[dataLocation[i].city.value] =
+                    dataLocation[i].region.friendly_name + ' ' +
+                    dataLocation[i].city.friendly_name;
+                continue;
+            }
+            locations[dataLocation[i].region.value] = null;
+            AddResearch.locations[dataLocation[i].region.value] =
+                dataLocation[i].region.friendly_name;
+        }
+        autocomplete.updateData(locations);
+    }
+
+    , sendResearch : async (event) => {
+        event.preventDefault();
         const serviceWorkerRegistration = await navigator.serviceWorker.ready;
         const subscription = await serviceWorkerRegistration.pushManager.getSubscription();
-        const addResearchForm = null || document.getElementById('add-research-form');
-        const headerTitle = null || document.getElementById('header-title');
+        const location = null || document.querySelector('#location');
+        const query = null || document.querySelector('#query');
+        const checkbox = null || document.querySelector('#only-title');
+        const endpoint = subscription.toJSON().endpoint;
 
-        headerTitle.innerText = 'Aggiungi una Ricerca';
+        let jsonForm = {
+            'location': Init.ucAll(location.value),
+            'location_parameters' : AddResearch.locations[Init.ucAll(location.value)],
+            'only_title' : checkbox.checked,
+            'query': query.value,
+            'endpoint': endpoint
+        };
 
-        addResearchForm.addEventListener('submit', async function (event) {
-            event.preventDefault();
-            const region = null || document.getElementById('region');
-            const city = null || document.getElementById('city');
-            const query = null || document.getElementById('query');
+        let jsonResponse = await ApiRequest.post(
+            '/add-research',
+            JSON.stringify(jsonForm)
+        );
 
-            const endpoint = subscription.toJSON().endpoint;
-            let jsonForm = {'region': region.value, 'city': city.value, 'query': query.value};
-
-            let jsonResponse = await ApiRequest.post(
-                '/add-research',
-                JSON.stringify(Object.assign(jsonForm, {endpoint}))
-            );
-
-            M.toast({html: jsonResponse.message});
-        });
+        M.toast({html: jsonResponse.message});
     }
 };
 
