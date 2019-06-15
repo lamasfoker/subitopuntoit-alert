dfbf90df807a1824543229219300b76cc1a533fb
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-06-15 11:37:17 +0200

    Make a Restyle

--- app/views/components/BottomBar.js
+++ app/views/components/BottomBar.js
@@ -6,7 +6,7 @@ let BottomBar = {
                 <nav class="top-nav fixed-bottom-bar">
                     <div class="container">
                         <div class="nav-wrapper">
-                            <div class="row">
+                            <div class="row menu">
                                 <div class="col s12 center-align">
                                     <a class="btn-flat" href="#/researches"><i class="material-icons">search</i></a>
                                     <a class="btn-flat" href="#/announcements"><i class="material-icons">message</i></a>
