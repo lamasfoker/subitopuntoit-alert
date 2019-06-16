b2f427185dbefb935aa3a83b230b85f3341c8572
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-06-16 15:25:18 +0200

    Refactor ApiRequest

--- app/services/ApiRequest.js
+++ app/services/ApiRequest.js
@@ -1,25 +1,27 @@
 const ApiRequest = {
-    //TODO: refactor the promise with await and parse the response
-
     post: async (url, body) => {
         let headers = new Headers();
         headers.set('Accept', 'application/json');
 
-        return await fetch(url, {
+        let response = await fetch(url, {
             method: 'POST',
             headers,
             body: body
-        }).then((response) => response.json());
+        });
+
+        return response.json();
     }
 
     , get: async (url) => {
         let headers = new Headers();
         headers.set('Accept', 'application/json');
 
-        return await fetch(url, {
+        let response = await fetch(url, {
             method: 'GET',
             headers
-        }).then((response) => response.json());
+        });
+
+        return response.json();
     }
 };
 
