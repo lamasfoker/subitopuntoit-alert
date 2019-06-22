0385dd6ac7f1b17cb5c042c3afc8609c158c69d4
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-06-22 16:17:56 +0200

    Refactor research.js

--- app/views/pages/Researches.js
+++ app/views/pages/Researches.js
@@ -10,6 +10,7 @@ let Researches = {
                     <a class="delete"><i class="material-icons circle blue">delete</i></a>
                     <span class="title"></span>
                     <p class="location"></p>
+                    <i class="material-icons secondary-content is-in-title">title</i>
                 </li>
             </ul>
         `
@@ -39,26 +40,42 @@ let Researches = {
 
         for (let i=0; i < jsonResponse.data.length; i++ ) {
             let research = jsonResponse.data[i];
+            let locationInfo = Researches.getLocationInfo(research.location_parameters.split(' ').length);
             let cln = listElement.cloneNode(true);
 
             cln.querySelector('.title').innerHTML = Utils.ucFirst(research.query);
-            cln.querySelector('.location').innerHTML = Utils.ucAll(research.location);
-            cln.querySelector('.delete').addEventListener('click', async () => {
-
-                let jsonResponse = await ApiRequest.post(
-                    '/delete-research',
-                    JSON.stringify(Object.assign(research, {endpoint}))
-                );
-
-                if (jsonResponse.code === 200) {
-                    cln.parentNode.removeChild(cln);
-                }
-                M.toast({html: jsonResponse.message});
+            cln.querySelector('.location').innerHTML = locationInfo+research.location;
+            if (research.is_only_in_title === false) {
+                cln.querySelector('.is-in-title').style.display = 'none';
+            }
+            cln.querySelector('.delete').addEventListener('click', () => {
+                Researches.deleteResearch(Object.assign(research, {endpoint}), cln);
             });
 
             document.getElementById("researches-list").appendChild(cln);
             cln.style.display = 'block';
         }
+    }
+
+    , getLocationInfo : (parametersNumber) => {
+        if (parametersNumber === 2) {
+            return 'Provincia di '
+        }
+        if (parametersNumber === 3) {
+            return 'Comune di '
+        }
+        return 'Regione ';
+    }
+
+    , deleteResearch : async (jsonBody, element) => {
+        let jsonResponse = await ApiRequest.post(
+            '/delete-research',
+            JSON.stringify(jsonBody)
+        );
+        if (jsonResponse.code === 200) {
+            element.parentNode.removeChild(element);
+        }
+        M.toast({html: jsonResponse.message});
     }
 };
 
