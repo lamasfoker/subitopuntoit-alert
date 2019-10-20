5f9e46703b64be16ae33cb7a3013e1849df0c3e7
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-10-20 16:46:54 +0200

    [BUGFIX] Make a request in the right way

--- server/service/database_cleaner.php
+++ server/service/database_cleaner.php
@@ -4,16 +4,19 @@ declare(strict_types=1);
 require __DIR__ . '/../../vendor/autoload.php';
 
 use SubitoPuntoItAlert\Database\Repository\AnnouncementRepository;
+use Symfony\Component\HttpClient\HttpClient;
 
 $announcementRepository = new AnnouncementRepository();
 $announcements = $announcementRepository->getAllAnnouncements();
+$client = HttpClient::create();
 
 foreach ($announcements as $announcement) {
     $jsonDetails = json_decode($announcement->getDetails(), true);
     if (array_key_exists('url', $jsonDetails)) {
         $url = $jsonDetails['url'];
-        $response = Requests::head($url);
-        if ($response->status_code !== 410) {
+
+        $response = $client->request('GET', $url);
+        if ($response->getStatusCode() !== 410) {
             continue;
         }
     }
