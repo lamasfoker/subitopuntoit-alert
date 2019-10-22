ec07a577dee0eb093e09e526ba3e89fa7caff9ef
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-10-22 20:01:58 +0200

    [REFACTOR] To handle all case

--- server/service/test_notification.php
+++ server/service/test_notification.php
@@ -5,9 +5,6 @@ use Minishlink\WebPush\WebPush;
 use Minishlink\WebPush\Subscription;
 use SubitoPuntoItAlert\Api\Response;
 
-// here I'll get the subscription endpoint in the POST parameters
-// but in reality, you'll get this information in your database
-// because you already stored it (cf. push_subscription.php)
 $subscription = Subscription::create(json_decode(file_get_contents('php://input'), true));
 $response = new Response();
 
@@ -25,15 +22,11 @@ $res = $webPush->sendNotification(
     $subscription,
     'Test Notification'
 );
-// TODO: deletes this code
-// handle eventual errors here, and remove the subscription from your server if it is expired
+
 foreach ($webPush->flush() as $report) {
     $endpoint = $report->getRequest()->getUri()->__toString();
 
-    if ($report->isSuccess()) {
-        $response->setHttpCode(200);
-        $response->setMessage("Message sent successfully for subscription {$endpoint}");
-    } else {
+    if (!$report->isSuccess()) {
         $response->setHttpCode(500);
         $response->setMessage("Message failed to sent for subscription {$endpoint}: {$report->getReason()}");
     }
@@ -43,6 +36,8 @@ foreach ($webPush->flush() as $report) {
         $response->setHttpCode(404);
         $response->setMessage("Subscription expired");
     }
+    $response->setHttpCode(200);
+    $response->setMessage("Message sent successfully for subscription {$endpoint}");
 }
 
 $response->send();
