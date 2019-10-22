272358640a02d2ec5cefe91a3a81afd6270dc5d7
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-10-22 22:39:17 +0200

    Delete month-old  announcements

--- server/service/database_cleaner.php
+++ server/service/database_cleaner.php
@@ -8,9 +8,18 @@ use Symfony\Component\HttpClient\HttpClient;
 
 $announcementRepository = new AnnouncementRepository();
 $client = HttpClient::create();
+date_default_timezone_set('Europe/Rome');
+$oneMonthAgo = date("Y-m-d H:i:s",strtotime("-1 months"));;
 
 foreach ($announcementRepository->getAnnouncements() as $announcement) {
     $jsonDetails = json_decode($announcement->getDetails(), true);
+    if (array_key_exists('date', $jsonDetails)) {
+        $date = $jsonDetails['date'];
+        if (strcmp($date, $oneMonthAgo) <= 0) {
+            $announcementRepository->delete($announcement);
+            continue;
+        }
+    }
     if (array_key_exists('url', $jsonDetails)) {
         $url = $jsonDetails['url'];
         $response = $client->request('HEAD', $url);
