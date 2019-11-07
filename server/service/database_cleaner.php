9077e71b0b9b51453d92811d0082e7fd2a85bd39
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-11-07 13:56:41 +0100

    Delete only announcements whit one or more months of life

--- server/service/database_cleaner.php
+++ server/service/database_cleaner.php
@@ -7,7 +7,6 @@ use SubitoPuntoItAlert\Database\Repository\AnnouncementRepository;
 use Symfony\Component\HttpClient\HttpClient;
 
 $announcementRepository = new AnnouncementRepository();
-$client = HttpClient::create();
 date_default_timezone_set('Europe/Rome');
 $oneMonthAgo = date("Y-m-d H:i:s",strtotime("-1 months"));;
 
@@ -17,15 +16,6 @@ foreach ($announcementRepository->getAnnouncements() as $announcement) {
         $date = $jsonDetails['date'];
         if (strcmp($date, $oneMonthAgo) <= 0) {
             $announcementRepository->delete($announcement);
-            continue;
         }
     }
-    if (array_key_exists('url', $jsonDetails)) {
-        $url = $jsonDetails['url'];
-        $response = $client->request('HEAD', $url, ['buffer' => false]);
-        if ($response->getStatusCode() !== 410) {
-            continue;
-        }
-    }
-    $announcementRepository->delete($announcement);
 }
